9278eacb54b5faaa8422217639adea7471de108a
Karthik Narasimhan <karthikatasu@gmail.com>
2019-03-28 20:50:53 -0400

    started authy using passport modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
--- client/src/app/follower.service.ts
+++ client/src/app/follower.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError, map, tap } from "rxjs/operators";
-import { Follower } from "./follower";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Follower } from './follower';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class FollowerService {
   constructor(private http: HttpClient) {}
 
-  url: string = "http://localhost:5000/follower/";
+  url = 'http://localhost:5000/follower/';
 
   getFollower(): Observable<Follower[]> {
     return this.http.get<Follower[]>(this.url);
