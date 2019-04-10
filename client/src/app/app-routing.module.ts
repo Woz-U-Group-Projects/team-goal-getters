3fbdac2ae6095785dfebfe1295b883c59cae98a0
Ziiiro <TerranceDS2@gmail.com>
2019-04-10 08:27:31 -0400

    Created dashboard component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FollowerSignup } from './components/follower-signup/follower-signup.component';
 import { InfluencerSignup } from './components/influencer-signup/influencer-signup.component';
 import { InfluencerLoginComponent } from './components/influencer-login/influencer-login.component';
+import { InfluencerDashboardComponent } from './components/influencer-dashboard/influencer-dashboard.component';
 import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   { path: 'influencer-signup', component: InfluencerSignup },
   { path: 'influencer-login', component: InfluencerLoginComponent },
+  { path: 'influencer-dashboard', component: InfluencerDashboardComponent },
   //{ path: 'follower/:id', component: FollowerDetailComponent },
   { path: 'follower-signup', component: FollowerSignup, data: { title: 'Followers List'}}
 ];
