5f3ad3d9ab1769180c9443521700a58331a11129
cgxix <cgomez@xixmia.com>
2019-04-12 15:59:59 -0400

    front-end routes, services, views

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FollowerSignup } from './components/follower-signup/follower-signup.component';
 import { InfluencerSignup } from './components/influencer-signup/influencer-signup.component';
-import { InfluencerLoginComponent } from './components/influencer-login/influencer-login.component';
-import { InfluencerDashboardComponent } from './components/influencer-dashboard/influencer-dashboard.component';
+import { InfluencerLogin } from './components/influencer-login/influencer-login.component';
+import { InfluencerDashboard } from './components/influencer-dashboard/influencer-dashboard.component';
 import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   { path: 'influencer-signup', component: InfluencerSignup },
-  { path: 'influencer-login', component: InfluencerLoginComponent },
-  { path: 'influencer-dashboard', component: InfluencerDashboardComponent },
-  //{ path: 'follower/:id', component: FollowerDetailComponent },
+  { path: 'influencer-login', component: InfluencerLogin },
+  { path: 'influencer-dashboard', component: InfluencerDashboard },
   { path: 'follower-signup', component: FollowerSignup, data: { title: 'Followers List'}}
 ];
 
