d80c4adf81dd813e706e19f0b42c0956552eec6f
cgxix <cgomez@xixmia.com>
2019-04-12 14:26:09 -0400

    install jwt, bcrypt, configured auth

--- models/influencer.js
+++ models/influencer.js
@@ -1,12 +1,18 @@
 var mongoose = require("mongoose");
-
-var Schema = mongoose.Schema;
+var bcrypt = require("bcryptjs");
 
 var InfluencerSchema = new Schema({
-  fname: { type: String, required: true },
-  lname: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true }
+});
+
+InfluencerSchema.pre("save", function(next) {
+  let salt = bcrypt.genSaltSync(10);
+  this.password = bcrypt.hashSync(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("Influencer", InfluencerSchema);
---
+++ services/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+// middleware method to validate a jwt token
+// add this as a second parameter to any requests that need token validatoin
+
+// router.get("route", authService.verifyUser, function(req,res,next){
+//   authorized work
+// });
+
+var authService = {
+  verifyUser: function(req, res, next) {
+    let token = req.cookies.jwt;
+    // if we have a cookie we can proceed
+    if (token) {
+      jwt.verify(token, "secretkey", function(err, decoded) {
+        if (err) {
+          res.json({ status: "error", message: err.message, data: null });
+        } else {
+          req.body.userId = decoded.id;
+          next();
+        }
+      });
+    }
+  }
+};
+
+module.exports = authService;
